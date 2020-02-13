90de3cc613e809fca98009b968680a8a3766406c
yuya.mohri <shion0430@gmail.com>
2020-02-13 07:46:23 +0000

    add status & DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_015251) do
+ActiveRecord::Schema.define(version: 2020_02_13_071601) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
