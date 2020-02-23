6a32e492fe2d6b45632de69445eb386ed6a7a771
yuya.mohri <shion0430@gmail.com>
2020-02-23 11:31:25 +0000

    correct signup_page, tasks_controller, routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :destroy]
+  
   
   def index
     @tasks = current_user.tasks.order(id: :desc)
@@ -52,5 +54,12 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to login_url
+    end
   end
 end
