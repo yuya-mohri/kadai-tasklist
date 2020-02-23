bb10765a8e1c395e160a84eb78e3bb42cff5c741
yuya.mohri <shion0430@gmail.com>
2020-02-23 01:53:07 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
-  
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only:[:create]
 end
